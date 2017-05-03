d447e06cf46af1c9b685be210405f59007501150
ricardo.ss <ricardo.ss@defensoria.to.gov.br>
2017-05-03 11:52:42 -0300

    Criando channel notificação que enviará notificação para os usuários

---
+++ app/channels/notification_channel.rb
@@ -0,0 +1,15 @@
+class NotificationChannel < ApplicationCable::Channel
+  def subscribed
+    # stream_from "some_channel"
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+
+  def listen
+  end
+
+  def speak
+  end
+end
