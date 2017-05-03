fc7bd2101b52f8a9941d26a352535b67602c262f
ricardo.ss <ricardo.ss@defensoria.to.gov.br>
2017-05-03 10:22:09 -0300

    Criando Scaffold para notificação

---
+++ db/migrate/20170503132045_create_notifications.rb
@@ -0,0 +1,13 @@
+class CreateNotifications < ActiveRecord::Migration[5.0]
+  def change
+    create_table :notifications do |t|
+      t.string :title
+      t.string :content
+      t.string :sender
+      t.string :recipient
+      t.string :link
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/notifications.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :notification do
+    title "MyString"
+    content "MyString"
+    sender "MyString"
+    recipient "MyString"
+    link "MyString"
+  end
+end
