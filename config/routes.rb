95950add726e9a703aad6558a331605082e11dbb
ricardo.ss <ricardo.ss@defensoria.to.gov.br>
2017-06-22 10:34:14 -0300

    Criação de rotas e arquivos de helper, controller e assests

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :accesses
   devise_for :users
   resources :notifications
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
