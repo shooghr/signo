6bcbad1407fc78b0af9f5d5069a2bdee26cb3107
ricardo.ss <ricardo.ss@defensoria.to.gov.br>
2017-06-30 15:27:44 -0300

    Adicionando rota de notificações vinculadas a usuario

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
 
   resources :accesses
   devise_for :users
-  resources :notifications
+
+  resources :users do
+    member do
+      resources :notifications
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
