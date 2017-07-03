73b2427d14129447c6adc5c932f2d715dd9cdd12
ricardo.ss <ricardo.ss@defensoria.to.gov.br>
2017-07-03 16:34:58 -0300

    Criando rota para redirecionamento para mensagem no sistema gerador

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,11 @@ Rails.application.routes.draw do
 
   resources :users do
     member do
-      resources :notifications
+      resources :notifications do
+        member do
+          get 'redirect' => 'notifications#redirect'
+        end
+      end
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
