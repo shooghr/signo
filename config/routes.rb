fc24c326be0224692d6f9467f8ca448d30e936a0
ricardo.ss <ricardo.ss@defensoria.to.gov.br>
2017-07-12 15:43:05 -0300

    Adicionando mount /cable

--- config/routes.rb
+++ config/routes.rb
@@ -20,5 +20,7 @@ Rails.application.routes.draw do
       end
     end
   end
+
+  mount ActionCable.server, at: '/cable'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
