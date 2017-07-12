016a0e8155f48a05f91c1378c2ac42e475708f59
ricardo.ss <ricardo.ss@defensoria.to.gov.br>
2017-07-12 11:51:10 -0300

    configurando action cable no pruduction.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   config.action_cable.mount_path = '/cable'
-  config.action_cable.url = 'wss://signo.defensoria.to.gov.br/cable'
+  config.action_cable.url = 'ws://signo.defensoria.to.gov.br'
   config.action_cable.allowed_request_origins = [ /http:\/\/*/, /https:\/\/*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
